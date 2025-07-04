6eae30969d6b1212a7f67ca84aa75195f32a180d
Nitin Gatla <gatlanit@gmail.com>
2025-07-04 14:35:59 -0500

    Added control descriptions

--- src/pages/Gain.jsx
+++ src/pages/Gain.jsx
@@ -8,14 +8,22 @@ export default function Gain() {
       <h1>What Is Gain?</h1>
       <p>
         Gain is basically how much you boost or reduce the strength (volume) of a sound. 
-        Think of it like a volume knob that controls how loud something gets before it moves through the rest of your audio chain.</p>
+        Think of it like a volume knob that controls how loud something gets before it moves through the rest of your audio chain.
+      </p>
+
+      <p>
+        Give it a try below! Drag the sphere in and out to change the gain amount. As you expand it, the amount of gain increases making it louder; shrinking it, on the otherhand, decreases the gain amount making it quieter.
+      </p>
+
+      <GainScene />
+      
       <p>
         There are different types of gain controls. 
         One of the most common is called <u>Channel Gain</u>. 
         This is used to adjust the volume of an individual track (like a guitar or vocal) so it fits better in the mix. 
         For example, you might turn up the gain on a quiet vocal to make it stand out more in a song.
       </p>
-      <GainScene />
+
       <p>
         Other types of gain, like <b>Makeup Gain</b> or <b>Preamp Gain</b>, work in similar ways but have extra "quirks". 
         Some can add color or warmth to the sound, while others just make it louder or quieter without impacting the tone. 
